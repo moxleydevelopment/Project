019ff86f2c2f5278f8da366c56e156e07d0954d9
Tim Wolf <timwolf1@gmail.com>
2020-10-19 14:23:35 -0400

    Removing unused imports.

--- src/main/java/Business/AddChiropractorServlet.java
+++ src/main/java/Business/AddChiropractorServlet.java
@@ -11,13 +11,11 @@ import java.io.PrintWriter;
 import java.sql.SQLException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 
 /**
  *
